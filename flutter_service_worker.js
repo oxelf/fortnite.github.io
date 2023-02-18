088b94b509315f7a566709dcad986ac8e9963daf
oxelf <110409182+oxelf@users.noreply.github.com>
2023-02-18 17:31:09 +0100

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "21114ae1f7bcd20eba1e420bffdcdbe3",
-"index.html": "bc339a1b69e2e0d1104b885ff05d2881",
-"/": "bc339a1b69e2e0d1104b885ff05d2881",
-"main.dart.js": "e6dfd5a7b43a5de972fcf956be93a0d9",
+"index.html": "c29a440bc12f108a0ce3f8cf8c8d5f28",
+"/": "c29a440bc12f108a0ce3f8cf8c8d5f28",
+"main.dart.js": "703add6c23d632381c71bb9d495f53e5",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
